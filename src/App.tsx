b9c65f4643bf21c56e1369d695474980b532d0b1
you <you@example.com>
2023-09-04 19:06:48 +0200

    commit client

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 // @ts-nocheck
 import { Routes, Route } from "react-router-dom";
 import Page from "./Page";
+import Page2 from "./Page2";
+import Page3 from "./Page3";
 function App() {
   return (
     <>
       <Routes>
         <Route index element={<Page />} />
-        <Route path="2" element={<Page />} />
-        <Route path="3" element={<Page />} />
+        <Route path="2" element={<Page2 />} />
+        <Route path="3" element={<Page3 />} />
       </Routes>
     </>
   );
